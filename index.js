b8ccd8f3d6c4fcd519b9f2406f9b4691f84e51eb
eisverticker <eisverticker@posteo.de>
2018-04-02 22:12:28 +0200

    Updated to es6

--- index.js
+++ index.js
@@ -1,64 +1,87 @@
-var prompt = require('prompt');
-var fs = require('fs');
-var nestedProperty = require("nested-property");
+const prompt = require("prompt");
+const fs = require("fs");
+const nestedProperty = require("nested-property");
 
-exports.configurator = function(configFiles, questions, additionalPatterns) {
+class ConsolePrinter {
+  static printHr() {
+    console.log("----------------------------------------");
+  }
 
-  /**
-   * Often used input patterns
-   */
-  var patterns = {
-    "email": {
-      pattern: /.{2,20}@.{2,20}\..{2,5}/,
-      message: 'Email must be valid'
-    },
-    "default": {},
-    "number": {
-      type: 'number'
-    },
-    "password": {
-      hidden: true
-    },
-    "boolean": {
-      type: 'boolean'
-    },
-    "array": {
-      type: 'array'
+  static printWelcome() {
+    console.log(" ");
+    console.log("==============================================================================");
+    console.log("Welcome to the configuration assistant, please provide us some informations");
+    console.log("==============================================================================");
+  }
+}
+
+class Configurator7 {
+  static getStandardPatterns() {
+    return {
+      "email": {
+        pattern: /.{2,20}@.{2,20}\..{2,5}/,
+        message: "Email must be valid"
+      },
+      "default": {},
+      "number": {
+        type: "number"
+      },
+      "password": {
+        hidden: true
+      },
+      "boolean": {
+        type: "boolean"
+      },
+      "array": {
+        type: "array"
+      }
+    };
+  }
+
+  constructor(configFiles, questions, additionalPatterns) {
+    this.configFiles = configFiles;
+    this.questions = questions;
+    this.patterns = Configurator7.getStandardPatterns();
+
+    /* optional parameter additionalPatterns */
+    if (typeof additionalPatterns !== undefined) {
+      for (let key in additionalPatterns) {
+        this.patterns[key] = additionalPatterns[key];
+      }
     }
-  };
+  }
 
-  if (additionalPatterns !== undefined) {
-    for (var key in additionalPatterns) {
-      patterns[key] = additionalPatterns[key];
-    }
+  run(callback){
+    this._init();
+
+    this._processQuestions(this.questions, function(err) {
+      if (err) callback(err);
+
+      console.log("Lets save the files!");
+      this._saveConfigurations(this._configFiles, callback);
+    });
   }
 
   /**
    * Welcome the user and initalize the prompt
    */
-  var init = function() {
-    isInit = true;
-
-    console.log(" ");
-    console.log("==============================================================================");
-    console.log("Welcome to the configuration assistant, please provide us some informations");
-    console.log("==============================================================================");
-
+  _init() {
+    ConsolePrinter.printWelcome();
     prompt.start();
-  };
+  }
 
   /**
    * Ask the user all questions you have provided
    */
-  var processQuestions = function(questions, callback) {
+  _processQuestions(questions, callback) {
 
     if (questions.length > 0) {
-      var q = questions.shift();
-      var schema = {
+      const q = questions.shift();
+      const schema = {
         properties: {}
       };
 
-      schema.properties.input = patterns[q.type];
+      schema.properties.input = this._patterns[q.type];
       schema.properties.input.required = q.required !== undefined ? q.required : true;
       if (q.default !== undefined) {
         schema.properties.input.default = q.default;
@@ -66,63 +89,50 @@ exports.configurator = function(configFiles, questions, additionalPatterns) {
 
       console.log(" ");
       console.log(q.title);
-      console.log("---------------------------------------------------------------------------");
+      ConsolePrinter.printHr();
+
       prompt.get(schema, function(err, response) {
         if (err) return callback(err);
 
-        var data = configFiles[q.target.file].data;
+        const data = this.configFiles[q.target.file].data;
 
         nestedProperty.set(data, q.target.position, response.input);
 
         //next question
-        processQuestions(questions, callback);
+        this._processQuestions(questions, callback);
       });
     } else {
       console.log("Thats it!");
       callback();
     }
-  };
+  }
 
   /**
    * Write the current data to disc
    */
-  var saveConfigurations = function(files, callback) {
+  _saveConfigurations(files, callback) {
 
-    var keys = Object.keys(files);
+    const keys = Object.keys(files);
 
     if (keys.length > 0) {
-      var key = keys.shift();
-      var file = files[key];
+      const key = keys.shift();
+      const file = files[key];
 
-      var data = JSON.stringify(file.data, null, 2);
+      const data = JSON.stringify(file.data, null, 2);
 
       fs.writeFile(file.targetFile, data, function(err) {
         if (err) return callback(err);
 
         console.log(file.targetFile + " was saved");
         delete files[key];
-        saveConfigurations(files, callback);
+        this._saveConfigurations(files, callback);
       });
     } else {
-      console.log("We have finished, thank you!");
+      console.log("Configuration was finished.");
       callback();
     }
-  };
+  }
 
+}
 
-  /**
-   * Start the configurator, ask the user and save all data which the user has given
-   */
-  this.run = function(callback) {
-		init();
-
-    processQuestions(questions, function(err) {
-      if (err) callback(err);
-
-      console.log("Lets save the files!");
-      saveConfigurations(configFiles, callback);
-    });
-
-  };
-
-};
+module.exports = Configurator7;
